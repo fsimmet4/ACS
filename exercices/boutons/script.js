59e720f26747b6346532ea14b77def4c9f002a36
Fsimmet2 <floriane.s@codeur.online>
2019-12-19 22:09:48 +0100

    index et correction

--- exercices/boutons/script.js
+++ exercices/boutons/script.js
@@ -1,20 +1,25 @@
-
-
+// Déclaration des variables, on récupere chaque bouton
 const blue = document.querySelector('.blue');
 const texte = document.querySelector('.texte');
 const alerte = document.querySelector('.alerte');
 
 
+// Ajout de l'événement clique au premier bouton
 blue.addEventListener('click', function(){
-    document.querySelector('body').style.backgroundColor = "blue";
+    // Changement de backgroundColor
+    document.querySelector('body').style.backgroundColor = "#6495ED";
 
 })
 
+// Ajout de l'événement clique au deuxième bouton
 texte.addEventListener('click', function(){
+    //Insertion d'un texte dans le HTML
     document.querySelector('.text').innerHTML = "coucou";
 })
 
+// Ajout de l'événement clique au troisième bouton
 alerte.addEventListener('click', function(){
-    alert('BRavo')
+    //alert
+    alert('Bravo')
     
 })
