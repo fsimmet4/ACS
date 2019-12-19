0d0393c5436a2dedb04cb078822833552ac280b9
Fsimmet2 <floriane.s@codeur.online>
2019-12-19 22:14:38 +0100

    correction script

--- exercices/div/script.js
+++ exercices/div/script.js
@@ -1,11 +1,16 @@
-let cases = document.querySelector('.case');
+//Déclaration des variables
+// Récuperation de la div 
+const cases = document.querySelector('.case');
+// Creation d'un compteur de clique
 let click = 0;
 
 
 
 cases.addEventListener("click", function () {
-    let res = "<h1 class='text-center'> Hello World! </h1>"
+    // Création du texte a afficher
+    const res = "<h1 class='text-center'> Hello World! </h1>"
 
+    // Switch pour chaque cas demandé
     switch (click) {
         case 0:
             cases.innerHTML = res;
@@ -21,5 +26,6 @@ cases.addEventListener("click", function () {
             cases.style.backgroundColor = "#698697";
             cases.innerHTML = "";
     }
+    // Incréméntation du compteur a chaque clique dans la div
     click += 1;
 })
