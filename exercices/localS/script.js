7529a7e971598d809ab641dfc317433d9c0f266a
Fsimmet2 <floriane.s@codeur.online>
2019-12-20 13:56:45 +0100

    Fonction creation de liste à partir d'u tableau

--- exercices/localS/script.js
+++ exercices/localS/script.js
@@ -7,3 +7,18 @@ window.onload = function(){
         localStorage.names = JSON.stringify(names);
     }
 }
+
+// Fonction qui permet de créer une liste à partir d'un tableau 
+function createListe(arr){
+
+    let liste = document.createElement('ul');
+    liste.classList = 'list-group liste';
+
+    for (let i = 0; i < arr.length; i++) {
+        let inside = document.createElement('li');
+        inside.classList = 'list-group-item'
+        inside.appendChild(document.createTextNode(arr[i]));
+        liste.appendChild(inside)
+    }
+    return liste;
+}
